65848378565756a11df02b2607baeb5e45399f00
Sinofipaza <fipazasino@gmail.com>
2023-07-26 06:32:17 -0700

    betty

---
+++ betty_checks.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *_getchar - gets character
+ *Return: character
+ */
+
+int _getchar(void)
+{
+	char c;
+
+	return (read(0, &c, 1) == 1 ? (unsigned char) c : E0F);
+}
