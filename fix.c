19ca2b1be7bfaf07c174f3eba23acaf90b00a93f
Sino <fipazasino@gmail.com>
2023-07-29 00:20:25 +0200

    Create fix.c

---
+++ fix.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints input string
+ * @str: string to be showed
+ * Return: nothing
+ */
+
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - shows the char c to stderr
+ * @c: character that show
+ * Return: one if runs
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - showscharacter c that will give to fd
+ * @c: character to print
+ * @fd: filedescriptor send to
+ * Return: one if runs
+ */
+
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - shows input string
+ * @str: string that will printed
+ * @fd: filedescriptor to send to
+ * Return: the number of chars put
+ */
+
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
