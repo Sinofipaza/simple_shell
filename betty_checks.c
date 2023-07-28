dab9cf82c1aa2659650844add465d594f9319d8e
Sinofipaza <fipazasino@gmail.com>
2023-07-28 04:38:21 -0700

    betty

--- betty_checks.c
+++ betty_checks.c
@@ -3,13 +3,13 @@
 
 /**
  *_getchar - gets character
- *Return: int value or E0F
+ *Return: int value or EOF
  */
 
 int _getchar(void)
 {
 	char c;
 
-	return (read(0, &c, 1) == 1 ? (unsigned char) c : E0F);
+	return (read(0, &c, 1) == 1 ? (unsigned char) c : EOF);
 	
 }
