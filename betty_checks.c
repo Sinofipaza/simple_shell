1605a359554b0da677ba83743767b7e27c264869
Sinofipaza <fipazasino@gmail.com>
2023-07-28 07:00:11 -0700

    betty

--- betty_checks.c
+++ betty_checks.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 
 /**
- *_getchar - gets character
- *Return: int value or EOF
+ * _getchar - gets character
+ * Return: int value or EOF
  */
-
 int _getchar(void)
 {
 	char c;
 
 	return (read(0, &c, 1) == 1 ? (unsigned char) c : EOF);
-	
 }
---
+++ strtok.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _free - free doubled character pointer
+ * @list: doubled character pointer
+ * @count: single character pointers number
+ */
+
+void _free(char **list, int count)
+{
+	for (; count >= 0; count--)
+		do_mem(0, list[count]);
+	do_mem(0, list);
+}
+
+/**
+ * _strtok - make string doubled character pointer
+ * @str: string you will split
+ * @delim: character used to split string
+ * Return: doubled charcter pointer
+ */
+
+char **_strtok(char *str, char *delim)
+{
+	int i = 0, j = 0, d = 0, len = 0, count = 0, check = 0;
+
+	char **list = NULL;
+
+	while(!(count = word_count(str, delim)))
+		return (NULL);
+	list = do_mem((count + 1) * sizeof(char *), NULL);
+	if (!list)
+		return (NULL);
+
+	for (i = 0, len = 0, count = 0; str[i] || len;)
+	{
+		if (((str[i] == delim[d]) || (!str[i])))
+		{
+			check += 1;
+			if (len)
+			{
+				list[count] = do_mem(sizeof(char) * (len + 1), NULL;
+				if (!list[count])
+				{
+					_free(list, count);
+					return (NULL);
+				}
+				for (j = 0; len; len--, j++)
+					list[count][j] = str[i - len];
+				list[count][j] = '\0';
+				count++;
+			}
