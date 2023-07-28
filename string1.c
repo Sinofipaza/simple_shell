f77f19698fd839928bb6ea60a07eef89a0cac0c8
Sino <fipazasino@gmail.com>
2023-07-28 23:16:23 +0200

    Update string1.c

--- string1.c
+++ string1.c
@@ -2,122 +2,63 @@
 #include <stdio.h>
 
 /**
- * _atoi - change string to int
- * @s: string to be changed
- * Return: integers
+ * is_delim - checks chars
+ * @c: chars to check
+ * @delim: delimeterr string
+ * Return: one when true
  */
-
-int _atoi(char *s)
+int is_delim(char c, char *delim)
 {
-	int num, len, numcount, mult, x, neg;
-
-	len = 0;
-	x = 0;
-	numcount = 0;
-	mult = 1;
-	neg = 1;
-	num = 0;
-
-	while (s[len] != '\0')
-	{
-		if (s[len] >= '0' && s[len] <= '9')
-		{
-			numcount++;
-			if (!(s[len + 1] >= '0' && s[len + 1] <= '9'))
-				break;
-		}
-		len++;
-	}
-	for (; numcount > 1; numcount--)
-		mult *= 10;
-	for (; x <= len; x++)
-	{
-		if (s[x] == '-')
-			neg *= -1;
-		else if (s[x] <= '9' && s[x] >= '0')
-		{
-			num += (s[x] - '0') * mult * neg;
-			mult /= 10;
-		}
-	}
-	return (num);
-}
-
-/**
- * sizeof_command - tells size
- * @tokens: tokenized
- * Return: command size
- */
-
-int sizeof_command(char **tokens)
-{
-	int i = 0;
-
-	if (!tokens)
-		return (0);
-	for (i = 0; tokens[i]; i++)
-	{
-		if (tokens[i] == '&' && tokens[i][1] == '&')
-			break;
-		if (tokens[i][0] == '|' && tokens[i][1] == '|')
-			break;
-		if (tokens[i][0] == '|' && tokens[i][1] == '|')
-			break;
-	}
-	return (i);
-}
-
-/**
- * _isdigit - digit
- * @c: checked digit
- * Return: 1 if digit
- */
-
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
+	while (*delim)
+	if (*delim++ == c)
 		return (1);
-
 	return (0);
 }
 
 /**
- * has_newline - checks for newline
- * @input: input
- * Return: EOF
+ * _isalpha - checks alphabetic chars
+ * @c: chars to input
+ * Return: one if c is alphabetic
  */
 
-int has_newline(char *input)
+int _isalpha(int c)
 {
-	int i;
-
-	for (i = 0; input && input[i]; i++)
-	{
-		if (input[i] == '\n')
-			return (i);
-	}
-	return (i);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 /**
- * shiftbufer - shifts buffer
- * @input: input
- * @newline_index: EOF
- * @filled: size it filled
+ * _atoi - converts stringgs to int
+ * @s: stringg converted
+ * Return: zero if no numbers in string
  */
 
-void shiftbuffer(char *input, int newline_index, int filled)
+int _atoi(char *s)
 {
-	int i = newline_index;
-	int j = 0;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-	for (; i < filled; i++, j++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		input[j] = input[i];
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
 	}
 
-	for (; j < 4096; j++)
-	{
-		input[j] = '\0';
-	}
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
